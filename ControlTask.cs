ac511573fa30ce555f28949b7b653815d985af67
Eatmeta <metagroul@gmail.com>
2022-11-07 15:13:30 +0300

    Implement a method ControlRocket that allows the rocket to aim itself at the target

--- ControlTask.cs
+++ ControlTask.cs
@@ -1,12 +1,18 @@
-using System;
-
 namespace func_rocket
 {
-	public class ControlTask
-	{
-		public static Turn ControlRocket(Rocket rocket, Vector target)
-		{
-			return Turn.None;
-		}
-	}
+    public class ControlTask
+    {
+        public static Turn ControlRocket(Rocket rocket, Vector target)
+        {
+            var a = 1.551;
+            var b = 2.45;
+            var c = 0.982;
+            
+            if ((a * rocket.Velocity.Angle + b * rocket.Direction) / (a + b) < c * (target - rocket.Location).Angle)
+                return Turn.Right;
+            return (a * rocket.Velocity.Angle + b * rocket.Direction) / (a + b) > c * (target - rocket.Location).Angle
+                ? Turn.Left
+                : Turn.None;
+        }
+    }
 }
